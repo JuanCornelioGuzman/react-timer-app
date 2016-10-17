d7ca73a261506b2c07c7545408ae2c4434bfb9b7
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2016-10-17 21:02:11 +0800

    Add Nav component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
+var Nav = require('Nav');
 
 var Main = (props) => {
   return (
     <div>
       <div>
+        <Nav/>
         <div>
           <p>Main.jsx Rendered</p>
           {props.children}
---
+++ app/components/Nav.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function(){
+    return(
+      <div>Nav.jsx</div>
+    )
+  }
+});
+
+module.exports = Nav;
